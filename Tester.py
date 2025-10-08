9cf389e253faedcd3eb7a7a7ad6288708c2e36f3
Lukka <51492470+Namakando-ApertureScience@users.noreply.github.com>
2025-10-09 01:33:03 +0200

    Visualisation mod.py

--- Tester.py
+++ Tester.py
@@ -1,6 +1,8 @@
 from Network_Constructor import Construct
 from Useful_methods import *
 
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
 
 # Do you want a file
 file = open("Training_data.txt", 'a') if (input("Would you like a file? (yes/no): ").lower()) == "yes" else None
@@ -16,25 +18,27 @@ if "patterned" == input("Would you like to train on random data or patterned dat
 else:
     pattern = False
 
-Data_set=None
+Data_set = None
+function = []
 if pattern:
     # Pattern function
-    function = [lambda arr: arr[0] + arr[1] + arr[2],
-                lambda arr: 2 * arr[0] - arr[1]]
+    function = [lambda arr: arr[0]**2 + 2,
+                lambda arr: 5**arr[0]]
 
     # Pattern data generation
-    Patterned_data_set = patterned_data_generator(function, 3, Number_of_datapoints=20)
+    Patterned_data_set = patterned_data_generator(function, 1, Number_of_datapoints=200, noise=0.05)
 
 else:
     # Random data
-    Data_set = random_data_generator(3, [-1, 1], 2, [0, 3], Number_of_datapoints=20)
+    Data_set = random_data_generator(2, [-1, 1], 2, [0, 3], Number_of_datapoints=20)
 
 ########################################################################################################################
 # Neural network sections #
 ###########################
 
 # Network parameters
-layers = [3, 10, 15, 2]                                                                                            # Options: layers (e.g. layers = [l1, l2, ...]), shape (e.g. shape = [depth, width])
+in_num, out_num = 1, 2
+layers = [in_num, 15, 15, out_num]                                                                                            # Options: layers (e.g. layers = [l1, l2, ...]), shape (e.g. shape = [depth, width])
 shape = [5, 6]
 approximation_interval = [-1, 4]
 
@@ -44,13 +48,36 @@ if Patterned_data_set:
 activation_functions = ["Sigmoid", "ReLU", ("Approximation_interval", approximation_interval)]                     # Can take sting of  1 AF or list of multiple. Options: ("Approximation_interval", approximation_interval), "Output_function", "Sigmoid", "ReLU"
 optimizers = "Adam"                                                                                                # Options: "Backpropagation", "Resilient_Backpropagation", "Adam"
 
-network = Construct(layers, activation_functions, optimizers, weight_decay_rate=0.001, learning_rate_adam=0.005)
+network = Construct(layers, activation_functions, optimizers, weight_decay_rate=0.0005, learning_rate_adam=0.01)
 
 ########################################################################################################################
 # Training #
 ############
 
 # training iterations
-iterations = 10_000
+iterations = 5_000
 
+network.copy_()
 train(pattern, Patterned_data_set, network, file, activation_functions, optimizers, iterations, Data_set)
+
+# visualisation
+training_data, testing_data = Patterned_data_set[:2]
+training_dataT, testing_dataT = list(zip(*training_data)), list(zip(*testing_data))
+
+if pattern and in_num == 1:
+
+    for index in range(len(function)):
+        buffer_1 = list(zip(*training_dataT[1]))
+        buffer_2 = list(zip(*testing_dataT[1]))
+
+        plot(training_dataT[0], testing_dataT[0], buffer_1[index], buffer_2[index])
+
+    net_out_old = list(zip(*[network.compute(x, old_eval=True) for x in testing_dataT[0]]))
+    for index in range(len(function)):
+        buffer_1 = list(zip(*training_dataT[1]))
+        plot(training_dataT[0], testing_dataT[0], buffer_1[index], net_out_old[index])
+
+    net_out = list(zip(*[network.compute(x) for x in testing_dataT[0]]))
+    for index in range(len(function)):
+        buffer_1 = list(zip(*training_dataT[1]))
+        plot(training_dataT[0], testing_dataT[0], buffer_1[index], net_out[index])
