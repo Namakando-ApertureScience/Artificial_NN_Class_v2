60a91cfa5e366f7ddfd59e45f475e42251b9d57e
Lukka <51492470+Namakando-ApertureScience@users.noreply.github.com>
2025-11-24 16:22:27 +0100

    Large scale update

--- Network_Constructor.py
+++ Network_Constructor.py
@@ -9,52 +9,61 @@ def derivative(func, x, quant=1e-13):
 
 class Activation_functions:
 
-    def __init__(self, steepness_value, approximation_interval=None, leaky_slope=0.01):
+    def __init__(self, slope, leaky_slope=0.05, approximation_interval=None):
         if approximation_interval is None:
             self.approximation_interval = [0, 1]
         else:
             self.approximation_interval = approximation_interval
 
-        self.steepness_value = steepness_value
+        self.slope = slope
         self.leaky_slope = leaky_slope
 
-    # Input normalized data and output within the approximation interval
+    # Input real data gets normalised and then, put within the approximation interval
     def Approximation_interval(self, x):
         return ((self.approximation_interval[1] - self.approximation_interval[0]) *
                 self.Sigmoid(x) + self.approximation_interval[0])
 
-    # Identity
+    # Identity (does nothing. xD)
     @staticmethod
-    def Output_function(x):
+    def Identity(x):
         return x
 
     # Input real data and output normalized data
     def Sigmoid(self, x):
         x = np.array(x, dtype=np.float128)
-        return 1 / (1 + np.e ** (-x / self.steepness_value))
-
-    def Hyperbolic_tangent(self):
-        return
-
-    # Input real data output non-linear data
-    def ReLU(self, x):
-        x = np.array(x, dtype=np.float128)
-        return np.array(list(map(lambda entry: max(0, entry), x * self.steepness_value)))
+        return 1 / (1 + np.e ** (-self.slope * x))
 
     # Input real data output non-linear data
     def Leaky_ReLU(self, x):
         x = np.array(x, dtype=np.float128)
-        return np.array(list(map(lambda entry: max(0, entry), x * self.steepness_value)))
+        return ((self.slope - self.leaky_slope) / 2) * (np.abs(x) + x) + self.leaky_slope * x
 
-    def Soft_max(self):
-        return
+    # Input real data output non-linear data
+    def ReLU(self, x):
+        self.leaky_slope = 0
+        return self.Leaky_ReLU(x)
+
+    # Input real data output non-linear data
+    def Softplus(self, x):
+        x = np.array(x, dtype=np.float128)
+        return self.slope * np.log(1 + np.exp(x))
+
+    # Input real data and output standardised data
+    def Hyperbolic_tangent(self, x):
+        return (np.exp(2 * self.slope * x) - 1) / (np.exp(2 * self.slope * x) + 1)
+
+    # Input real data and output normalised exponential data
+    @staticmethod
+    def Softmax(x):
+        return np.exp(x) / (np.exp(x).sum())
+
 
 class Construct:
 
     def __init__(self, layers=None, str_activation_functions=None, optimizers=None, shape=None,
                  weight_interval=None, input_output_steepness=None, weight_decay_rate=0.000_100,
                  learning_rate_BP=0.1, learning_rate_up=1.05, learning_rate_down=0.5,
-                 learning_rate_adam=0.001, rho1=0.9, rho2=0.999):
+                 learning_rate_adam=0.001, rho1=0.9, rho2=0.999, Loss_function="MSE"):
 
         ################################################################################################################
         # Neural network constructor #
@@ -65,51 +74,43 @@ class Construct:
             raise Exception("Necessary parameters missing.")
 
         if layers is None:
-            layers = [shape[1] for i in range(shape[0])]
+            layers = [shape[1] for _ in range(shape[0])]
 
         self.layers = layers
 
         if input_output_steepness is None:
-            self.steepness_values = np.full(len(layers) - 1, 1)
+            self.slopes = np.full(len(layers) - 1, 1)
         else:
-            self.steepness_values = np.linspace(input_output_steepness[0], input_output_steepness[1], len(layers) - 1)
+            self.slopes = np.linspace(input_output_steepness[0], input_output_steepness[1], len(layers) - 1)
 
         if str_activation_functions is None:
-            str_activation_functions = ["Sigmoid" for i in layers][:-1]
+            str_activation_functions = ["Sigmoid" for _ in layers][:-1]
         elif type(str_activation_functions) is not list:
-            str_activation_functions = [str_activation_functions for i in layers][:-1]
+            str_activation_functions = [str_activation_functions for _ in layers][:-1]
 
         if weight_interval is None:
             weight_interval = [-0.5, 0.5]
 
         if optimizers is None:
-            optimizers = ["Backpropagation" for i in layers][:-1]
+            optimizers = ["Backpropagation" for _ in layers][:-1]
         elif type(optimizers) is not list:
-            optimizers = [optimizers for i in layers][:-1]
+            optimizers = [optimizers for _ in layers][:-1]
 
         self.optimizers = optimizers
 
         # Activation functions
         self.activation_functions = []
 
-        for index in range(len(layers) - 1):
-            if type(str_activation_functions[index]) is tuple:
-                if str_activation_functions[index][0] == "Approximation_interval":
-                    self.activation_functions.append(Activation_functions(
-                        steepness_value=self.steepness_values[index],
-                        approximation_interval=str_activation_functions[index][1]).Approximation_interval)
+        for _, act_spec, slope in zip(layers[:-1], str_activation_functions, self.slopes):
+            if isinstance(act_spec, tuple) and act_spec[0] == "Approximation_interval":
+                activation = Activation_functions(
+                    approximation_interval=act_spec[1],
+                    slope=slope,
+                ).Approximation_interval
             else:
-                if str_activation_functions[index] == "Output_function":
-                    self.activation_functions.append(Activation_functions(
-                        steepness_value=self.steepness_values[index]).Output_function)
+                activation = getattr(Activation_functions(slope=slope), act_spec)
 
-                if str_activation_functions[index] == "Sigmoid":
-                    self.activation_functions.append(Activation_functions(
-                        steepness_value=self.steepness_values[index]).Sigmoid)
-
-                if str_activation_functions[index] == "ReLU":
-                    self.activation_functions.append(Activation_functions(
-                        steepness_value=self.steepness_values[index]).ReLU)
+            self.activation_functions.append(activation)
 
         # Important matrices
         self.neural_network_copy = []
@@ -122,6 +123,14 @@ class Construct:
                                        np.random.rand(layers[index], layers[index - 1] + 1) + weight_interval[0])
 
         self.neural_network_copy = self.neural_network.copy()
+
+        ################################################################################################################
+        # Losses constructor #
+        ######################
+
+        self.Loss_function = self.MSE if Loss_function == "MSE" else (
+            self.Cross_entropy if Loss_function == "Cross_entropy" else (
+                None))
 
         ################################################################################################################
         # optimizers constructor #
@@ -175,6 +184,30 @@ class Construct:
             self.Adam_Init()
 
     ####################################################################################################################
+    # Loss functions #
+    ##################
+
+    def del_(self):
+        return derivative(self.activation_functions[len(self.layers) - 2],
+                          self.weighted_sums_matrix[len(self.layers) - 2])
+
+    # MSE
+    def MSE(self, X_point, y_point):
+        dif = y_point - self.forward(X_point)           # First
+        del_ = self.del_()                              # Second (Order is important !!!)
+        return dif * del_
+
+    # Cross-entropy
+    def Cross_entropy(self, X_point, y_point):
+        logits = self.forward(X_point)                  # First
+        del_ = self.del_()                              # Second (Order is important !!!)
+
+        p = np.exp(logits) / (np.exp(logits).sum())
+        class_vec = (y_point - p) * del_
+
+        return class_vec.sum()
+
+    ####################################################################################################################
     # Optimizer Initialization #
     ############################
 
@@ -213,40 +246,6 @@ class Construct:
             self.time_step_bool[index] = True
 
     ####################################################################################################################
-    # Neural network #
-    ##################
-
-    def compute(self, input_vector, old_eval=False):
-
-        # Bias neuron
-        input_vector = input_vector.copy()
-        input_vector.append(1)
-
-        if old_eval:
-            vector = np.matmul(self.neural_network_copy[0], input_vector)
-
-            for index in range(1, len(self.neural_network)):
-                vector = np.matmul(self.neural_network_copy[index],
-                                   np.append(self.activation_functions[index - 1](vector), 1))
-            return self.activation_functions[-1](vector)
-
-        else:
-            self.weighted_sums_matrix = []
-            self.weighted_sums_matrix.append(np.matmul(self.neural_network[0], input_vector))
-
-            for index in range(1, len(self.neural_network)):
-                self.weighted_sums_matrix.append(np.matmul(self.neural_network[index],
-                np.append(self.activation_functions[index - 1](self.weighted_sums_matrix[-1]), 1)))
-            return self.activation_functions[-1](self.weighted_sums_matrix[-1])
-
-    def sum(self):
-        return sum([sum([sum([abs(weight) for weight in neuron])
-                         for neuron in layer]) for layer in self.neural_network])
-
-    def copy_(self):
-        self.neural_network_copy = self.neural_network.copy()
-
-    ####################################################################################################################
     # optimizer #
     #############
 
@@ -267,43 +266,6 @@ class Construct:
             self.Adam_Init()
 
     ####################################################################################################################
-    # Optimizer
-
-    def Optimize(self, input_vector, target_output_vector):
-        self.error_vector = target_output_vector - self.compute(input_vector)
-
-        gradient = derivative(self.activation_functions[len(self.layers) - 2],
-                              self.weighted_sums_matrix[len(self.layers) - 2]) * self.error_vector
-        self.distributer(gradient)
-
-        for index in range(len(self.layers) - 3, 0, -1):
-            gradient = (derivative(self.activation_functions[index], self.weighted_sums_matrix[index]) *
-                        np.matmul(np.delete(self.neural_network[index + 1].transpose(),
-                                            self.layers[index + 1], axis=0), gradient))
-            self.distributer(gradient, index)
-
-    ####################################################################################################################
-    # Distributer
-
-    def distributer(self, gradient, index=None):
-        if index is None:
-            index = len(self.layers) - 2
-
-        if self.optimizers[index] == "Adam" and self.time_step_bool[index]:
-            self.time_step[index] += 1
-            self.bias_correction[index] = (m.sqrt(1 - self.rho2 ** self.time_step[index]) /
-                                           (1 - self.rho1 ** self.time_step[index]))
-
-            if abs(self.bias_correction[index] - 1) < self.epsilon:
-                self.time_step_bool[index] = False
-                self.bias_correction[index] = 1
-
-        (self.Backpropagation_prop_update(gradient, index) if self.optimizers[index] == "Backpropagation"
-         else (self.Resilient_prop_update(gradient, index) if self.optimizers[index] == "Resilient_Backpropagation"
-               else (self.Adam_update(gradient, index) if self.optimizers[index] == "Adam"
-                     else print("Error no optimizers specified!!!"))))
-
-    ####################################################################################################################
     # Backpropagation
 
     def Backpropagation_prop_update(self, gradient, index):
@@ -319,10 +281,11 @@ class Construct:
             self.weighted_sums_matrix[index - 1]), 1)]))
 
         self.ILG[index][1] = self.ILG[index][1] * ((np.sign(gradient * self.ILG[index][2]) *
-                                                    (self.learning_rate_up - self.learning_rate_down) + abs(
+                                                    (self.learning_rate_up - self.learning_rate_down) + np.abs(
                     np.sign(gradient *
                             self.ILG[index][2])) * (self.learning_rate_up + self.learning_rate_down)) / 2 +
-                                                   abs(self.ILG[index][0] - abs(np.sign(gradient * self.ILG[index][2]))))
+                                                   np.abs(self.ILG[index][0] - np.abs(
+                                                       np.sign(gradient * self.ILG[index][2]))))
 
         self.ILG[index][2] = gradients
 
@@ -344,3 +307,75 @@ class Construct:
                                        (self.learning_rate_adam * self.bias_correction[index]) *
                                        (self.AF[index][1] / (self.AF[index][0] + self.epsilon) ** 0.5)) -
                                       self.weight_decay_rate * self.neural_network[index])
+
+    ####################################################################################################################
+    # Distributer
+
+    def distributer(self, gradient, index=None):
+        if index is None:
+            index = len(self.layers) - 2
+
+        if self.optimizers[index] == "Adam" and self.time_step_bool[index]:
+            self.time_step[index] += 1
+            self.bias_correction[index] = (m.sqrt(1 - self.rho2 ** self.time_step[index]) /
+                                           (1 - self.rho1 ** self.time_step[index]))
+
+            if np.abs(self.bias_correction[index] - 1) < self.epsilon:
+                self.time_step_bool[index] = False
+                self.bias_correction[index] = 1
+
+        (self.Backpropagation_prop_update(gradient, index) if self.optimizers[index] == "Backpropagation"
+         else (self.Resilient_prop_update(gradient, index) if self.optimizers[index] == "Resilient_Backpropagation"
+               else (self.Adam_update(gradient, index) if self.optimizers[index] == "Adam"
+                     else print("Error no optimizers specified!!!"))))
+
+    ####################################################################################################################
+    # Optimizer
+
+    def Optimize(self, X, y):
+
+        gradient = (sum(
+            [self.Loss_function(X_point, y_point) for X_point, y_point in zip(X, y)]
+                ))/len(X)
+
+        self.distributer(gradient)
+
+        for index in range(len(self.layers) - 3, 0, -1):
+            gradient = (derivative(self.activation_functions[index], self.weighted_sums_matrix[index]) *
+                        np.matmul(np.delete(self.neural_network[index + 1].transpose(),
+                                            self.layers[index + 1], axis=0), gradient))
+            self.distributer(gradient, index)
+
+    ####################################################################################################################
+    # Neural network #
+    ##################
+
+    def forward(self, input_vector, old_eval=False):
+        # Bias neuron
+        input_vector = input_vector.copy()
+        input_vector.append(1)
+
+        if old_eval:
+            vector = np.matmul(self.neural_network_copy[0], input_vector)
+
+            for index in range(1, len(self.neural_network)):
+                vector = np.matmul(self.neural_network_copy[index],
+                                   np.append(self.activation_functions[index - 1](vector), 1))
+            return self.activation_functions[-1](vector)
+
+        else:
+            self.weighted_sums_matrix = []
+            self.weighted_sums_matrix.append(np.matmul(self.neural_network[0], input_vector))
+
+            for index in range(1, len(self.neural_network)):
+                self.weighted_sums_matrix.append(np.matmul(self.neural_network[index],
+                                                           np.append(self.activation_functions[index - 1](
+                                                               self.weighted_sums_matrix[-1]), 1)))
+            return self.activation_functions[-1](self.weighted_sums_matrix[-1])
+
+    def sum(self):
+        return sum([sum([sum([np.abs(weight) for weight in neuron])
+                         for neuron in layer]) for layer in self.neural_network])
+
+    def copy_(self):
+        self.neural_network_copy = self.neural_network.copy()
